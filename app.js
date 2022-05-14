4434b7c503aa335a0de7fcef54e5c44ea5588327
sohail60 <sohailatwork10@gmail.com>
2022-05-14 13:16:12 +0530

    Modify my Code to remove bug

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.get("/",function(req,res){
     app.post("/loc",function(req,res){
         const city=req.body.city;
         console.log(city);
-        res.write(city);
+        // res.write(city);
         const apiKey="2175fc5eadf92e42daeabaff456a55e8";
         const url="https://api.openweathermap.org/data/2.5/weather?q="+ city +"&units=metric&appid="+apiKey;
 
@@ -27,15 +27,16 @@ app.get("/",function(req,res){
                 const desc=weatherData.weather[0].description;
                 const icon=weatherData.weather[0].icon;
 
-
+                // Wrong Code
                 // const iconUrl="https://openweathermap.org/img/wn/"+ icon +"@2x.png";
                 // res.write("<h1>The temp in " + city + " is: " +temp+ "</h1>");
                 // res.write("<p>The weather currently is: " +desc+ "</p>");
                 // res.write("<img src=" + iconUrl+ ">");
 
+                // Right Code
                 const iconUrl = "https://openweathermap.org/img/wn/" + icon + "@2x.png";
-                res.setHeader("Content-Type", "text/html");
-                res.write("<h1>The temp in " + city + " is: " + temp + "</h1>", "utf8");
+                res.setHeader("Content-Type", "text/html");     // necessary to write before using res.write
+                res.write("<h1>The temp in " + city + " is: " + temp + "</h1>", "utf8");    //encoding(utf8) is necessary in res.write
                 res.write("<p>The weather currently is: " + desc + "</p>", "utf8");
                 res.write("<img src=" + iconUrl + ">", "utf8");
 
